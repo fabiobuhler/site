f49bc710a2f3d076f9d11c5096423e9ce45dfb12
fabiobuhler <125405884+fabiobuhler@users.noreply.github.com>
2024-09-12 01:24:50 -0300

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Pega os dados do formulário
+    $nome = $_POST['name'];
+    $email = $_POST['email'];
+    $telefone = $_POST['phone'];
+    $mensagem = $_POST['message'];
+
+    // Endereço de e-mail para onde os dados serão enviados
+    $to = "seuemail@dominio.com"; // Substitua pelo seu e-mail
+    $subject = "Novo contato de Lünler Planejados";
+    $body = "Nome: $nome\nEmail: $email\nTelefone: $telefone\nMensagem: $mensagem";
+
+    // Cabeçalhos de e-mail
+    $headers = "From: $email";
+
+    // Envia o e-mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Falha no envio da mensagem.";
+    }
+}
+?>
